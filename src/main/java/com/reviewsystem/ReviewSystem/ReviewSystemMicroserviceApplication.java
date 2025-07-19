03b58784671ec0b09c44ae40514b6b95d1788004
Tarun Arora <gobelinus@users.noreply.github.com>
2025-07-19 13:57:03 +0530

    🏗️ Boilerplate code generated via sprintboot web
    ref: https://start.spring.io/

---
+++ src/main/java/com/reviewsystem/ReviewSystem/ReviewSystemMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.reviewsystem.ReviewSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSystemMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewSystemMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reviewsystem/ReviewSystem/ReviewSystemMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewsystem.ReviewSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewSystemMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
